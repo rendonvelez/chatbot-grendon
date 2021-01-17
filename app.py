38c0f916da75b5eb0147f4b2fda16cd9c594ddee
Grendon <rendon.velez@hotmail.com>
2021-01-17 17:40:52 -0500

    first commit

--- app.py
+++ app.py
@@ -9,24 +9,6 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import requests
 from flask import Flask, request
-from keras.models import load_model
-
-training_model = load_model('training_model.h5')
-encoder_inputs = training_model.input[0]
-encoder_outputs, state_h_enc, state_c_enc = training_model.layers[2].output
-encoder_states = [state_h_enc, state_c_enc]
-encoder_model = Model(encoder_inputs, encoder_states)
-
-latent_dim = 256
-decoder_state_input_hidden = Input(shape=(latent_dim,))
-decoder_state_input_cell = Input(shape=(latent_dim,))
-decoder_states_inputs = [decoder_state_input_hidden, decoder_state_input_cell]
-decoder_outputs, state_hidden, state_cell = decoder_lstm(
-    decoder_inputs, initial_state=decoder_states_inputs)
-decoder_states = [state_hidden, state_cell]
-decoder_outputs = decoder_dense(decoder_outputs)
-decoder_model = Model([decoder_inputs] + decoder_states_inputs,
-                      [decoder_outputs] + decoder_states)
 
 spacy.load("en_core_web_sm")
 app = Flask(__name__)
