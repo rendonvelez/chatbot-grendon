575fd9ff1c556b4de1da9c14bd9de2ab09974359
Grendon <rendon.velez@hotmail.com>
2021-01-24 17:04:43 -0500

    re

--- app.py
+++ app.py
@@ -26,6 +26,42 @@ lines2 = [" ".join(re.findall(r"\w+", line.lower())) for line in lines2]
 pairs = list(zip(lines, lines2))
 # random.shuffle(pairs)
 
+input_docs = []
+target_docs = []
+input_tokens = set()
+target_tokens = set()
+for line in pairs:
+    input_doc, target_doc = line[0], line[1]
+    # Appending each input sentence to input_docs
+    input_docs.append(input_doc)
+    # Splitting words from punctuation
+    target_doc = " ".join(re.findall(r"[\w']+|[^\s\w]", target_doc))
+    # Redefine target_doc below and append it to target_docs
+    target_doc = '<START> ' + target_doc + ' <END>'
+    target_docs.append(target_doc)
+
+    # Now we split up each sentence into words and add each unique word to our vocabulary set
+    for token in re.findall(r"[\w']+|[^\s\w]", input_doc):
+        if token not in input_tokens:
+            input_tokens.add(token)
+    for token in target_doc.split():
+        if token not in target_tokens:
+            target_tokens.add(token)
+
+input_tokens = sorted(list(input_tokens))
+target_tokens = sorted(list(target_tokens))
+num_encoder_tokens = len(input_tokens)
+num_decoder_tokens = len(target_tokens)
+
+input_features_dict = dict(
+    [(token, i) for i, token in enumerate(input_tokens)])
+target_features_dict = dict(
+    [(token, i) for i, token in enumerate(target_tokens)])
+reverse_input_features_dict = dict(
+    (i, token) for token, i in input_features_dict.items())
+reverse_target_features_dict = dict(
+    (i, token) for token, i in target_features_dict.items())
+
 training_model = tf.keras.models.load_model('model/training_model.h5')
 
 app = Flask(__name__)
