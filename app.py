23a2af3d56a77eb08cf19a5dba6f91daa941541a
Grendon <rendon.velez@hotmail.com>
2021-01-24 18:13:03 -0500

    App

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ import json
 import requests
 import numpy as np
 import tensorflow as tf
+import tensorflow as tf
+from keras.layers import Input, LSTM, Dense
+from keras.models import Model
 
 from flask import Flask, request
 
@@ -69,7 +72,72 @@ max_encoder_seq_length = max(
 max_decoder_seq_length = max(
     [len(re.findall(r"[\w']+|[^\s\w]", target_doc)) for target_doc in target_docs])
 
+# Dimensionality
+dimensionality = 256
+# The batch size and number of epochs
+batch_size = 5
+epochs = 600
+# Encoder
+encoder_inputs = Input(shape=(None, num_encoder_tokens))
+encoder_lstm = LSTM(dimensionality, return_state=True)
+encoder_outputs, state_hidden, state_cell = encoder_lstm(encoder_inputs)
+encoder_states = [state_hidden, state_cell]
+# Decoder
+decoder_inputs = Input(shape=(None, num_decoder_tokens))
+decoder_lstm = LSTM(dimensionality, return_sequences=True, return_state=True)
+decoder_outputs, decoder_state_hidden, decoder_state_cell = decoder_lstm(
+    decoder_inputs, initial_state=encoder_states)
+decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+
 training_model = tf.keras.models.load_model('model/training_model.h5')
+encoder_inputs = training_model.input[0]
+encoder_outputs, state_h_enc, state_c_enc = training_model.layers[2].output
+encoder_states = [state_h_enc, state_c_enc]
+encoder_model = Model(encoder_inputs, encoder_states)
+
+latent_dim = 256
+decoder_state_input_hidden = Input(shape=(latent_dim,))
+decoder_state_input_cell = Input(shape=(latent_dim,))
+decoder_states_inputs = [decoder_state_input_hidden, decoder_state_input_cell]
+decoder_outputs, state_hidden, state_cell = decoder_lstm(
+    decoder_inputs, initial_state=decoder_states_inputs)
+decoder_states = [state_hidden, state_cell]
+decoder_outputs = decoder_dense(decoder_outputs)
+decoder_model = Model([decoder_inputs] + decoder_states_inputs,
+                      [decoder_outputs] + decoder_states)
+
+
+def decode_response(test_input):
+    # Getting the output states to pass into the decoder
+    states_value = encoder_model.predict(test_input)
+    # Generating empty target sequence of length 1
+    target_seq = np.zeros((1, 1, num_decoder_tokens))
+    # Setting the first token of target sequence with the start token
+    target_seq[0, 0, target_features_dict['<START>']] = 1.
+
+    # A variable to store our response word by word
+    decoded_sentence = ''
+
+    stop_condition = False
+    while not stop_condition:
+        # Predicting output tokens with probabilities and states
+        output_tokens, hidden_state, cell_state = decoder_model.predict(
+            [target_seq] + states_value)
+    # Choosing the one with highest probability
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_token = reverse_target_features_dict[sampled_token_index]
+        decoded_sentence += " " + sampled_token
+    # Stop if hit max length or found the stop token
+        if (sampled_token == '<END>' or len(decoded_sentence) > max_decoder_seq_length):
+            stop_condition = True
+    # Update the target sequence
+        target_seq = np.zeros((1, 1, num_decoder_tokens))
+        target_seq[0, 0, sampled_token_index] = 1.
+        # Update states
+        states_value = [hidden_state, cell_state]
+    return decoded_sentence
+
 
 app = Flask(__name__)
 
