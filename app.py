198da5c1420545bb5d0bb3fcbbb84867b5b151c7
Grendon <rendon.velez@hotmail.com>
2021-01-24 21:02:29 -0500

    App

--- app.py
+++ app.py
@@ -13,10 +13,100 @@ from keras.layers import Input, LSTM, Dense
 
 from flask import Flask, request
 
+data_path = "model/human_text.txt"
+data_path2 = "model/robot_text.txt"
+# Defining lines as a list of each line
+with open(data_path, 'r', encoding='utf-8') as f:
+    lines = f.read().split('\n')
+with open(data_path2, 'r', encoding='utf-8') as f:
+    lines2 = f.read().split('\n')
+lines = [re.sub(r"\[\w+\]", 'hola', line.lower()) for line in lines]
+lines = [" ".join(re.findall(r"\w+", line.lower())) for line in lines]
+lines2 = [re.sub(r"\[\w+\]", '', line.lower()) for line in lines2]
+lines2 = [" ".join(re.findall(r"\w+", line.lower())) for line in lines2]
+# grouping lines by response pair
+pairs = list(zip(lines, lines2))
+# random.shuffle(pairs)
+
+input_docs = []
+target_docs = []
+input_tokens = set()
+target_tokens = set()
+
+for line in pairs:
+    input_doc, target_doc = line[0], line[1]
+    # Appending each input sentence to input_docs
+    input_docs.append(input_doc)
+    # Splitting words from punctuation
+    target_doc = " ".join(re.findall(r"[\w']+|[^\s\w]", target_doc))
+    # Redefine target_doc below and append it to target_docs
+    target_doc = '<START> ' + target_doc + ' <END>'
+    target_docs.append(target_doc)
+
+    # Now we split up each sentence into words and add each unique word to our vocabulary set
+    for token in re.findall(r"[\w']+|[^\s\w]", input_doc):
+        if token not in input_tokens:
+            input_tokens.add(token)
+    for token in target_doc.split():
+        if token not in target_tokens:
+            target_tokens.add(token)
+
+input_tokens = sorted(list(input_tokens))
+target_tokens = sorted(list(target_tokens))
+num_encoder_tokens = len(input_tokens)
+num_decoder_tokens = len(target_tokens)
+
+input_features_dict = dict(
+    [(token, i) for i, token in enumerate(input_tokens)])
+target_features_dict = dict(
+    [(token, i) for i, token in enumerate(target_tokens)])
+reverse_input_features_dict = dict(
+    (i, token) for token, i in input_features_dict.items())
+reverse_target_features_dict = dict(
+    (i, token) for token, i in target_features_dict.items())
+
+# Maximum length of sentences in input and target documents
+max_encoder_seq_length = max(
+    [len(re.findall(r"[\w']+|[^\s\w]", input_doc)) for input_doc in input_docs])
+max_decoder_seq_length = max(
+    [len(re.findall(r"[\w']+|[^\s\w]", target_doc)) for target_doc in target_docs])
+
+# Dimensionality
+dimensionality = 256
+
+# Encoder
+encoder_inputs = Input(shape=(None, num_encoder_tokens))
+encoder_lstm = LSTM(dimensionality, return_state=True)
+encoder_outputs, state_hidden, state_cell = encoder_lstm(encoder_inputs)
+encoder_states = [state_hidden, state_cell]
+# Decoder
+decoder_inputs = Input(shape=(None, num_decoder_tokens))
+decoder_lstm = LSTM(dimensionality, return_sequences=True, return_state=True)
+decoder_outputs, decoder_state_hidden, decoder_state_cell = decoder_lstm(
+    decoder_inputs, initial_state=encoder_states)
+decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+training_model = load_model('model/training_model.h5')
+encoder_inputs = training_model.input[0]
+encoder_outputs, state_h_enc, state_c_enc = training_model.layers[2].output
+encoder_states = [state_h_enc, state_c_enc]
+encoder_model = Model(encoder_inputs, encoder_states)
+
+latent_dim = 256
+decoder_state_input_hidden = Input(shape=(latent_dim,))
+decoder_state_input_cell = Input(shape=(latent_dim,))
+decoder_states_inputs = [decoder_state_input_hidden, decoder_state_input_cell]
+decoder_outputs, state_hidden, state_cell = decoder_lstm(
+    decoder_inputs, initial_state=decoder_states_inputs)
+decoder_states = [state_hidden, state_cell]
+decoder_outputs = decoder_dense(decoder_outputs)
+decoder_model = Model([decoder_inputs] + decoder_states_inputs,
+                      [decoder_outputs] + decoder_states)
+
 negative_responses = ("no", "negativo", "nada", "nop", "nopi")
 exit_commands = ("chao", "adios", "nos vemos",
                  "suerte", "chaito", "bye", "chaolin")
-
 
 app = Flask(__name__)
 
@@ -44,7 +134,7 @@ def webhook():
 
     data = request.get_json()
 
-    # log(data)  # logging, no necesario en produccion
+    log(data)  # logging, no necesario en produccion
 
     inteligente = True
 
@@ -174,98 +264,4 @@ def log(message):  # funcion de logging para heroku
 
 
 if __name__ == '__main__':
-
-    data_path = "model/human_text.txt"
-    data_path2 = "model/robot_text.txt"
-    # Defining lines as a list of each line
-    with open(data_path, 'r', encoding='utf-8') as f:
-        lines = f.read().split('\n')
-    with open(data_path2, 'r', encoding='utf-8') as f:
-        lines2 = f.read().split('\n')
-    lines = [re.sub(r"\[\w+\]", 'hola', line.lower()) for line in lines]
-    lines = [" ".join(re.findall(r"\w+", line.lower())) for line in lines]
-    lines2 = [re.sub(r"\[\w+\]", '', line.lower()) for line in lines2]
-    lines2 = [" ".join(re.findall(r"\w+", line.lower())) for line in lines2]
-    # grouping lines by response pair
-    pairs = list(zip(lines, lines2))
-    # random.shuffle(pairs)
-
-    input_docs = []
-    target_docs = []
-    input_tokens = set()
-    target_tokens = set()
-
-    for line in pairs:
-        input_doc, target_doc = line[0], line[1]
-        # Appending each input sentence to input_docs
-        input_docs.append(input_doc)
-        # Splitting words from punctuation
-        target_doc = " ".join(re.findall(r"[\w']+|[^\s\w]", target_doc))
-        # Redefine target_doc below and append it to target_docs
-        target_doc = '<START> ' + target_doc + ' <END>'
-        target_docs.append(target_doc)
-
-        # Now we split up each sentence into words and add each unique word to our vocabulary set
-        for token in re.findall(r"[\w']+|[^\s\w]", input_doc):
-            if token not in input_tokens:
-                input_tokens.add(token)
-        for token in target_doc.split():
-            if token not in target_tokens:
-                target_tokens.add(token)
-
-    input_tokens = sorted(list(input_tokens))
-    target_tokens = sorted(list(target_tokens))
-    num_encoder_tokens = len(input_tokens)
-    num_decoder_tokens = len(target_tokens)
-
-    input_features_dict = dict(
-        [(token, i) for i, token in enumerate(input_tokens)])
-    target_features_dict = dict(
-        [(token, i) for i, token in enumerate(target_tokens)])
-    reverse_input_features_dict = dict(
-        (i, token) for token, i in input_features_dict.items())
-    reverse_target_features_dict = dict(
-        (i, token) for token, i in target_features_dict.items())
-
-    # Maximum length of sentences in input and target documents
-    max_encoder_seq_length = max(
-        [len(re.findall(r"[\w']+|[^\s\w]", input_doc)) for input_doc in input_docs])
-    max_decoder_seq_length = max(
-        [len(re.findall(r"[\w']+|[^\s\w]", target_doc)) for target_doc in target_docs])
-
-    # Dimensionality
-    dimensionality = 256
-
-    # Encoder
-    encoder_inputs = Input(shape=(None, num_encoder_tokens))
-    encoder_lstm = LSTM(dimensionality, return_state=True)
-    encoder_outputs, state_hidden, state_cell = encoder_lstm(encoder_inputs)
-    encoder_states = [state_hidden, state_cell]
-    # Decoder
-    decoder_inputs = Input(shape=(None, num_decoder_tokens))
-    decoder_lstm = LSTM(
-        dimensionality, return_sequences=True, return_state=True)
-    decoder_outputs, decoder_state_hidden, decoder_state_cell = decoder_lstm(
-        decoder_inputs, initial_state=encoder_states)
-    decoder_dense = Dense(num_decoder_tokens, activation='softmax')
-    decoder_outputs = decoder_dense(decoder_outputs)
-
-    training_model = load_model('model/training_model.h5')
-    encoder_inputs = training_model.input[0]
-    encoder_outputs, state_h_enc, state_c_enc = training_model.layers[2].output
-    encoder_states = [state_h_enc, state_c_enc]
-    encoder_model = Model(encoder_inputs, encoder_states)
-
-    latent_dim = 256
-    decoder_state_input_hidden = Input(shape=(latent_dim,))
-    decoder_state_input_cell = Input(shape=(latent_dim,))
-    decoder_states_inputs = [
-        decoder_state_input_hidden, decoder_state_input_cell]
-    decoder_outputs, state_hidden, state_cell = decoder_lstm(
-        decoder_inputs, initial_state=decoder_states_inputs)
-    decoder_states = [state_hidden, state_cell]
-    decoder_outputs = decoder_dense(decoder_outputs)
-    decoder_model = Model([decoder_inputs] + decoder_states_inputs,
-                          [decoder_outputs] + decoder_states)
-
     app.run(debug=True)
