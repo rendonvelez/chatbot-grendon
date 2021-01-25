85350e2c765a37779eb4cbb36e1a78f9f3a4d5b9
Grendon <rendon.velez@hotmail.com>
2021-01-24 20:04:12 -0500

    app

--- app.py
+++ app.py
@@ -228,25 +228,29 @@ def decode_response(test_input):
     #     states_value = [hidden_state, cell_state]
     return decoded_sentence
 
+# Method to convert user input into a matrix
 
-def string_to_matrix(user_input):
+
+def string_to_matrix(self, user_input):
     tokens = re.findall(r"[\w']+|[^\s\w]", user_input)
     user_input_matrix = np.zeros(
-        (1, max_encoder_seq_length, num_encoder_tokens), dtype='float32')
+        (1, max_encoder_seq_length, num_encoder_tokens),
+        dtype='float32')
     for timestep, token in enumerate(tokens):
         if token in input_features_dict:
             user_input_matrix[0, timestep, input_features_dict[token]] = 1.
-        return user_input_matrix
+    return user_input_matrix
+
+# Method that will create a response using seq2seq model we built
 
 
-def generate_response(user_input):
-    input_matrix = string_to_matrix(user_input)
+def generate_response(self, user_input):
+    input_matrix = self.string_to_matrix(user_input)
     chatbot_response = decode_response(input_matrix)
     # Remove <START> and <END> tokens from chatbot_response
     chatbot_response = chatbot_response.replace("<START>", '')
     chatbot_response = chatbot_response.replace("<END>", '')
     return chatbot_response
-    # Method to check for exit commands
 
 
 def make_exit(reply):
